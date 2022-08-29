73aaba41b69a9eb64fdc227b2a4ed54a0559f17d
lisdude <git@lisdude.com>
2022-08-29 15:39:55 -0500

    Add a potential fix for DPI scaling issues.

--- C3DSLinuxInstaller/README.md
+++ C3DSLinuxInstaller/README.md
@@ -1,11 +1,15 @@
 This script will 'install' Docking Station for Linux. And by 'install' I mean create a DockingStation directory that you can launch the game from.
 
-Usage:
+## Usage
 1. Download a copy of `dockingstation_195_64.tar.bz2` into the same directory as the script.
 2. If you want to play Creatures 3 docked or stand-alone, copy your full `Creatures 3` directory into the same place as the script. WARNING: Part of the installation involves lowercasing all file names, so your C3 directory will be altered.
 3. Run the script: `./install_dockingstation.sh`
 
 The script itself only has one configuration option, and that's where to 'install' to. Feel free to change it or just move the `DockingStation` directory after the fact.
 
-Caveats:
-- Sound doesn't seem to be working.
+## Scaling Issues
+Linux is weird about DPI scaling, so you may find the window is teeny-tiny. If this is the case, there's another script here called `dpi_fix.sh` that may help. Just run it after your normal installation. (If you know a better option for scaling, please let me know!)
+
+## Caveats
+- Sound seems to be hit or miss.
+- Some virtual machines had issues with the arrow keys when using the DPI fix.
---
+++ C3DSLinuxInstaller/dpi_fix.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+# This script will attempt to fix scaling issues for users of high DPI displays. It uses xpra and a 'run_scaled' script.
+# By default, because of outdated Ubuntu packages, it uses the beta version of xpra. If you don't want that, just comment out
+# or delete everything from "# BETA STARTS HERE" to "# BETA ENDS HERE"
+
+INSTALL_DIR="./DockingStation"
+
+WEBSITE="https://github.com/Xpra-org/xpra/wiki/Download"
+INSTALL_DIR=$(realpath ${INSTALL_DIR})
+
+if ! [[ -d "${INSTALL_DIR}" ]]; then
+    echo "Couldn't find a Docking Station installation. If you changed the default location, you'll need to edit this script and change INSTALL_DIR at the top."
+    exit
+fi
+
+if [[ -r /etc/os-release ]]; then
+    . /etc/os-release
+    DISTRO=${UBUNTU_CODENAME}
+    if [[ "${DISTRO}" == "" ]]; then
+        echo "Couldn't determine your distribution's codename. You'll need to manually install the xpra beta from: ${WEBSITE}"
+        exit
+    fi
+else
+    echo "Not running a distribution with /etc/os-release available. You'll need to manually install the xpra beta from: ${WEBSITE}"
+    exit
+fi
+
+### This script is adapted from the one provided at ${WEBSITE} (see above)
+
+echo "Installing the beta version of xpra for '${DISTRO}'..."
+
+# BETA STARTS HERE
+#install https support for apt (which may be installed already):
+sudo apt update
+sudo apt install apt-transport-https software-properties-common
+sudo apt install ca-certificates
+# add Xpra GPG key(s)
+sudo wget -O "/usr/share/keyrings/xpra-2022.gpg" https://xpra.org/xpra-2022.gpg
+# older distributions may also need:
+sudo wget -O "/usr/share/keyrings/xpra-2018.gpg" https://xpra.org/xpra-2018.gpg
+# add beta channel:
+sudo wget -O "/etc/apt/sources.list.d/xpra-beta.list" https://xpra.org/repos/$DISTRO/xpra-beta.list
+# BETA ENDS HERE
+
+# install Xpra package
+sudo apt update
+sudo apt install xpra xvfb python3-pyinotify python3-uinput
+
+# Having this installed made everything blurry in my VM. But real hardware? Unknown!
+if pip freeze | grep "PyOpenGL-accelerate"=; then
+    echo ""
+    while true; do
+        read -p "PyOpenGL-accelerate, in my experience, makes everything blurry. If you know you don't need it, you can uninstall it. If you're not sure, probably best to say no here. Uninstall? (y/n)" yn
+        case $yn in
+            [yY] ) pip uninstall PyOpenGL PyOpenGL-accelerate;
+                break;;
+            [nN] ) echo "Leaving it alone!";
+                break;;
+            * ) echo "Not sure what you want, so leaving it alone.";;
+        esac
+    done
+fi
+
+echo "Installing run_scaled and modifying launcher(s)..."
+cd "${INSTALL_DIR}"
+wget https://raw.githubusercontent.com/kaueraal/run_scaled/master/run_scaled 2>/dev/null
+chmod u+x ./run_scaled
+
+for game in ds c3; do
+    if [[ -f "${INSTALL_DIR}/${game}" ]]; then
+        ed "${INSTALL_DIR}/${game}" <<EOF
+/^\.\/lc2e/c
+./run_scaled --scale=2 ./lc2e --autokill
+.
+wq
+EOF
+    fi
+done
+
+echo "Done!"
